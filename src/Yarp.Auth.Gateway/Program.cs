d021098637a48be1895343c2d91434e28033c397
Burak Gokoglu <burak.gokoglu@eversana.com>
2024-08-04 19:12:34 -0500

    initial commit

---
+++ src/Yarp.Auth.Gateway/Program.cs
@@ -0,0 +1,44 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.BearerToken;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services
+    .AddAuthentication(BearerTokenDefaults.AuthenticationScheme)
+    .AddBearerToken();
+
+builder.Services
+    .AddAuthorization(o =>
+    {
+        o.AddPolicy("first-api-access",
+            policy => policy.RequireAuthenticatedUser().RequireClaim("first-api-access", true.ToString()));
+
+        o.AddPolicy("second-api-access",
+            policy => policy.RequireAuthenticatedUser().RequireClaim("second-api-access", true.ToString()));
+    });
+
+var app = builder.Build();
+
+app.MapGet("login", (bool firstApi = false, bool secondApi = false) =>
+    Results.SignIn(
+        new ClaimsPrincipal(
+            new ClaimsIdentity(
+                [
+                    new Claim("sub", Guid.NewGuid().ToString()),
+                    new Claim("first-api-access", firstApi.ToString()),
+                    new Claim("second-api-access", secondApi.ToString())
+                ],
+                BearerTokenDefaults.AuthenticationScheme)),
+        authenticationScheme: BearerTokenDefaults.AuthenticationScheme));
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapReverseProxy();
+
+app.Run();
