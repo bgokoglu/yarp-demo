9ea3c4ce71ab01f648e0337e63a15020495b6966
Burak Gokoglu <burak.gokoglu@eversana.com>
2024-08-05 20:16:26 -0500

    add serilog

--- src/Yarp.Auth.Api/Program.cs
+++ src/Yarp.Auth.Api/Program.cs
@@ -1,7 +1,20 @@
 using Asp.Versioning;
 using Asp.Versioning.Builder;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.ClearProviders();
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    //.MinimumLevel.Override("Microsoft", LogEventLevel.Error)
+    //.MinimumLevel.Override("System", LogEventLevel.Error)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateLogger();
+
+builder.Services.AddSerilog();
 
 builder.Services.AddApiVersioning(options =>
 {
@@ -26,20 +39,36 @@ ApiVersionSet apiVersionSet = app.NewApiVersionSet()
     .Build();
 
 // Route without version in URL, allowing versioning through header or default
-app.MapGet("api/hello", () => "Hello from downstream API v1 version")
+app.MapGet("api/hello", () =>
+    {
+        Log.Logger.Information("Hello from downstream API v1 version");
+        return "Hello from downstream API v1 version";
+    })
     .WithApiVersionSet(apiVersionSet)
     .MapToApiVersion(1);
 
-app.MapGet("api/hello", () => "Hello from downstream API v2 version")
+app.MapGet("api/hello", () =>
+    {
+        Log.Logger.Information("Hello from downstream API v2 version");
+        return "Hello from downstream API v2 version";
+    })
     .WithApiVersionSet(apiVersionSet)
     .MapToApiVersion(2);
 
 // Existing routes with version in URL for backward compatibility
-app.MapGet("api/v{version:apiVersion}/hello", () => "Hello from downstream API v1 version")
+app.MapGet("api/v{version:apiVersion}/hello", () =>
+    {
+        Log.Logger.Information("Hello from downstream API v1 version");
+        return "Hello from downstream API v1 version";
+    })
     .WithApiVersionSet(apiVersionSet)
     .MapToApiVersion(1);
 
-app.MapGet("api/v{version:apiVersion}/hello", () => "Hello from downstream API v2 version")
+app.MapGet("api/v{version:apiVersion}/hello", () =>
+    {
+        Log.Logger.Information("Hello from downstream API v2 version");
+        return "Hello from downstream API v2 version";
+    })
     .WithApiVersionSet(apiVersionSet)
     .MapToApiVersion(2);
 
